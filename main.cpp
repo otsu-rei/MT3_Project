52189b1f7c64895554eca669bebc766f48e3a4ba
otsu-rei <k023g0090@g.neec.ac.jp>
2024-04-12 11:23:47 +0900

    default push

--- main.cpp
+++ main.cpp
@@ -20,12 +20,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, kWindowWidth, kWindowHeight);
 
-	Vector3f v1 = { 1.0f, 3.0f, -5.0f };
-	Vector3f v2 = { 4.0f, -1.0f, 2.0f };
-	float k = 4.0f;
-
-	const int kRowHeight = 20;
-
 	/***********************************
 	 * ゲームループ *
 	 ***********************************/
@@ -48,13 +42,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-
-		VectorNovice::ScreenPrintf(0, 0,  v1 + v2, ": Add");
-		VectorNovice::ScreenPrintf(0, kRowHeight, v1 - v2, ": Subtract");
-		VectorNovice::ScreenPrintf(0, kRowHeight * 2, v1 * k, ": Multiply");
-		Novice::ScreenPrintf(0, kRowHeight * 3, "%.02f : Dot", Vector::Dot(v1, v2));
-		Novice::ScreenPrintf(0, kRowHeight * 4, "%.02f : Length", Vector::Length(v1));
-		VectorNovice::ScreenPrintf(0, kRowHeight * 5, Vector::Normalize(v2), ": Normalize");
 
 		///
 		/// ↑描画処理ここまで
